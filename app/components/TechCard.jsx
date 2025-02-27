72c8e9b19ce16e6dce8bad8016e6dd790f153e79
Santos Alarcón <santosalarcon86@gmail.com>
2025-02-27 12:06:04 +0100

    seo: añadidas las etiquetas alt a las imagenes

--- app/components/TechCard.jsx
+++ app/components/TechCard.jsx
@@ -1,7 +1,7 @@
 const TechCard = ({ image, title }) => {
     return (
         <div className="m-2 flex h-24 w-24 justify-center items-center cursor-pointer rounded-2xl border-white transition-all duration-300 hover:rotate-12 hover:scale-125 hover:drop-shadow-[0_0_1em_#66f]">
-            <img src={image} title={title} loading="lazy" />
+            <img alt={title} src={image} title={title} loading="lazy" />
         </div>
     );
 };
