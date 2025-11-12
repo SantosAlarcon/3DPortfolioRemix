8b63982e44da1c640c95a798a816c75a28147595
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2025-11-12 13:10:15 +0100

    style: cambio de anchura a 92rem

--- app/hoc/SectionWrapper.jsx
+++ app/hoc/SectionWrapper.jsx
@@ -10,7 +10,7 @@ const SectionWrapper = (Component, idName) =>
                 initial="hidden"
                 whileInView="show"
                 viewport={{ once: true, amount: 0.05 }}
-                className={`${styles.padding} relative z-0 mx-auto max-w-7xl`}
+                className={`${styles.padding} relative z-0 mx-auto max-w-[92rem]`}
             >
                 <span className="hash-span" id={idName}>
                     &nbsp;
